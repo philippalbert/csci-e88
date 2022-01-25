d413de96a94c3c858d0d7c71f42c4835ee3c2d4a
Philippsons <philipp.albert1504@gmail.com>
2022-01-25 21:44:52 +0100

    Add __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,7 @@
+from src.utils import create_file, get_lines_and_keys, write_sorted_list, get_max_and_min_key
+
+if __name__ == '__main__':
+    # create_file()
+    write_sorted_list()
+    minimum, maximum = get_max_and_min_key()
+    print(minimum, maximum)
